f73bdcfe3b1f5f7a61cffa462f32662e9fdfff7e
Dennis Yu <dennisyu319@gmail.com>
2013-02-12 13:39:22 -0500

    Update example.rb

--- example.rb
+++ example.rb
@@ -5,3 +5,5 @@ new = getDataFromDesk('new')
 active = getDataFromDesk('new,open')
 awaitingDev = getDataFromDesk('pending', 'Awaiting Dev')
 pending = getDataFromDesk('pending') - awaitingDev
+
+pushDataToGecko('https://push.geckoboard.com/v1/send/example', apikey, '"item":[{"text":"<p>New: #{new}</p><p> Active: #{active}/#{total}</p><p>Dev: #{awaitingDev}</p><p>Pending: #{pending}/#{total}","type":0}]')
